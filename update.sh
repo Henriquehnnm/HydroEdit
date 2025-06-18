99f2dff0d69b17c96f007a9ae209a0bfa9288696
Henriquehnnm <henriquesardinha987@gmail.com>
2025-06-18 15:33:33 -0300

    Update

--- update.sh
+++ update.sh
@@ -72,7 +72,7 @@ main() {
     
     # Baixa a nova versão
     info_msg "Baixando a nova versão..."
-    if wget -q https://raw.githubusercontent.com/Henriquehnnm/HydroEdit/main/hydroedit.py -O "$HOME/.hydroedit.py"; then
+       if wget https://raw.githubusercontent.com/Henriquehnnm/HydroEdit/main/hydroedit.py -O "$HOME/.hydroedit.py"; then
         chmod +x "$HOME/.hydroedit.py"
         success_msg "HydroEdit atualizado com sucesso!"
         info_msg "Um backup do arquivo anterior foi salvo em: $backup_file"
